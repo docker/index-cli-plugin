40d55445bd1847f7abfe458b1518b1ac0a989b84
Christian Dupuis <cd@atomist.com>
2022-10-29 17:07:27 +0200

    Read version from binary
    Signed-off-by: Christian Dupuis <cd@atomist.com>

--- sbom/detect/node.go
+++ sbom/detect/node.go
@@ -17,14 +17,24 @@
 package detect
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"strconv"
 	"strings"
 
 	"github.com/anchore/syft/syft/source"
 	"github.com/docker/index-cli-plugin/types"
 )
 
-func nodePackageDetector(_ []types.Package, image source.Source, lm types.LayerMapping) []types.Package {
+func nodePackageDetector(pkgs []types.Package, image source.Source, lm types.LayerMapping) []types.Package {
+	// Already found nodejs via package manager
+	for _, p := range pkgs {
+		if purl, err := types.ToPackageUrl(p.Purl); err == nil && purl.Name == "nodejs" {
+			return []types.Package{}
+		}
+	}
+
 	var path []string
 	var nodeVersion string
 
@@ -40,12 +50,25 @@ func nodePackageDetector(_ []types.Package, image source.Source, lm types.LayerM
 		}
 	}
 
-	if nodeVersion != "" && len(path) > 0 {
+	if len(path) > 0 {
 		res, _ := image.FileResolver(source.SquashedScope)
 		for _, p := range path {
 			fp := fmt.Sprintf("%s/node", p)
 			if locations, err := res.FilesByPath(fp); err == nil && len(locations) > 0 {
 				loc := locations[0]
+
+				if nodeVersion == "" {
+					f, _ := res.FileContentsByLocation(loc)
+					values := readStrings(f, "node.js/v")
+					if len(values) == 1 {
+						nodeVersion = values[0][9:]
+					}
+				}
+
+				if nodeVersion == "" {
+					continue
+				}
+
 				return []types.Package{{
 					Type:        "github",
 					Namespace:   "nodejs",
@@ -67,4 +90,49 @@ func nodePackageDetector(_ []types.Package, image source.Source, lm types.LayerM
 	}
 
 	return []types.Package{}
+}
+
+var (
+	min   = 6
+	max   = 256
+	ascii = true
+)
+
+func readStrings(file io.ReadCloser, prefix string) []string {
+	detected := make([]string, 0)
+	in := bufio.NewReader(file)
+	str := make([]rune, 0, max)
+	filePos := int64(0)
+	print := func() {
+		if len(str) >= min {
+			s := string(str)
+			if strings.HasPrefix(s, prefix) {
+				detected = append(detected, s)
+			}
+		}
+		str = str[0:0]
+	}
+	for {
+		var (
+			r   rune
+			wid int
+			err error
+		)
+		for ; ; filePos += int64(wid) {
+			r, wid, err = in.ReadRune()
+			if err != nil {
+				return detected
+			}
+			if !strconv.IsPrint(r) || ascii && r >= 0xFF {
+				print()
+				continue
+			}
+			// It's printable. Keep it.
+			if len(str) >= max {
+				print()
+			}
+			str = append(str, r)
+		}
+	}
+	return detected
 }
